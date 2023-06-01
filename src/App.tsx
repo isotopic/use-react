d7eaa037e1728d432879acee717548ba19f6f80f
Guilherme Cruz <guilhermecruz@gmail.com>
2023-06-01 10:49:46 -0300

    Ready, set, go

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import toolbox from '/images/toolbox.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={toolbox} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
